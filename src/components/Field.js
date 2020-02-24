5a5feb9b8a43b3a4e07f8e6fc8680a013e644837
unknown <renaud.dubuis@decryptage.net>
2020-02-24 13:00:00 +0100

    Field Component Internal Logic And Communication

--- src/components/Field.js
+++ src/components/Field.js
@@ -2,16 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Field(props) {
-  const { value, rule, errorMessage } = props;
+  const { value, rule, errorMessage, actionCallback, placeholder } = props;
 
   // Create Internal State with React.useState( initialValue ) "Hook"
   const [inputValue, setInpuValue] = React.useState(value);
+  let valid = rule.test(inputValue);
 
-  const inputChangeHandler = (event) => {
+  const inputChangeHandler = event => {
     // Call to setter "setInpuValue" re render component
     setInpuValue(event.target.value);
   };
- 
+
+  const inputBlurHandler = event => {
+    valid && actionCallback(inputValue);
+  }
+
   const inputStyles = {
     margin: "5px",
     maxWidth: "99%"
@@ -25,21 +30,29 @@ function Field(props) {
         style={inputStyles}
         value={inputValue}
         onChange={inputChangeHandler}
+        onBlur={inputBlurHandler}
+        placeholder={placeholder}
       />
-      <div className="alert alert-danger" style={inputStyles}>
-        <strong>{errorMessage}</strong>
-      </div>
+      {!valid && (
+        <div className="alert alert-danger" style={inputStyles}>
+          <strong>{errorMessage}</strong>
+        </div>
+      )}
     </React.Fragment>
   );
 }
 Field.defaultProps = {
   value: "test",
   rule: /.*/,
-  errorMessage: "Invalid Value"
+  errorMessage: "Invalid Value",
+  actionCallback:()=>false,
+  placeholder:''
 };
 
 Field.propTypes = {
+  actionCallback: PropTypes.func,
   value: PropTypes.string,
+  placeholder: PropTypes.string,
   rule: PropTypes.instanceOf(RegExp),
   errorMessage: PropTypes.string
 };
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,30 +1,38 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Range from './Range';
-import Field from './Field';
-import Button from './Button';
+import React from "react";
+import PropTypes from "prop-types";
+import Range from "./Range";
+import Field from "./Field";
+import Button from "./Button";
 
 function Question(props) {
-    const {} = props
+  const {} = props;
 
-    // Handler is a naming convention for Event Function
-    const buttonClickHandler = () => { // Arrow Functions "=>" declares a function
-        console.log('Button Clicked !')
-    }
+  // Handler is a naming convention for Event Function
+  const buttonClickHandler = () => {
+    // Arrow Functions "=>" declares a function
+    console.log("Button Clicked !");
+  };
 
-    return (
-        <div>
-            <Range></Range>
-            <Field ></Field>
+  const nameFiledHandler = name => alert("Welcome " + name);
 
-            <Button color="success" actionCallback={buttonClickHandler}>My Text</Button>
- 
-        </div>
-    )
+  return (
+    <div>
+      <Range></Range>
+      
+      <Field
+        errorMessage="Too Many Characters !"
+        rule={/^\w{0,8}$/}
+        actionCallback={nameFiledHandler}
+        placeholder="name"
+      ></Field>
+
+      <Button color="success" actionCallback={buttonClickHandler}>
+        My Text
+      </Button>
+    </div>
+  );
 }
 
-Question.propTypes = {
+Question.propTypes = {};
 
-}
-
-export default Question
+export default Question;
