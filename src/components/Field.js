9e7c3f325d9f9b469c56041da9123fb2e0cf029e
unknown <renaud.dubuis@decryptage.net>
2020-02-24 12:27:45 +0100

    Field Componet Props Deifnition

--- src/components/Field.js
+++ src/components/Field.js
@@ -4,6 +4,8 @@ import PropTypes from "prop-types";
 function Field(props) {
   const {} = props;
 
+
+
   const inputStyles = {
     margin: "5px",
     maxWidth:'99%'
@@ -12,12 +14,22 @@ function Field(props) {
   // React.Fragment is a virtual wrapper not rendered in the DOM
   return (
     <React.Fragment>
-      <input className="form-control" style={inputStyles}/>
+      <input className="form-control" style={inputStyles} />
       <div className="alert alert-danger" style={inputStyles}><strong>Invalid Value</strong></div>
     </React.Fragment>
   );
 }
+Field.defaultProps = {
+    value:'',
+    rule:/.*/,
+    errorMessage:"Invalid Value",  
+};
 
-Field.propTypes = {};
+
+Field.propTypes = {
+    value:PropTypes.string,
+    rule:PropTypes.instanceOf(RegExp),
+    errorMessage:PropTypes.string,
+};
 
 export default Field;
